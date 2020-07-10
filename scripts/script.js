d0b490d8bf4ae6547750181bfffdc091d6315c7a
Vyacheslav <korzan.va@mail.ru>
2020-07-10 22:13:20 +0800

    2 lesson

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,16 @@ let arrStr = [
 ].join(`\n`);
 
 
+// console.log(window.location.hash); // Получить хэш от ссылки .html#open = open
+// console.log(window.location.host); // 
+console.log(window.location);
+
+
 // --->>>>  START <<<< --- //
+
+// Здесь будем хранить все объявления
+const dataBase = [];
+
 
 const
   // Модалка
@@ -18,30 +27,33 @@ const
   addAd = document.querySelector(`.add__ad`),
   modalBtnSubmit = document.querySelector(`.modal__btn-submit`),
   modalSubmit = document.querySelector(`.modal__submit`),
+  modalBtnWarning = document.querySelector(`.modal__btn-warning`),
 
   // Каталог 
   catalog = document.querySelector(`.catalog`),
   modalItem = document.querySelector(`.modal__item`);
 
 
+
+
+
 // Закрытие модальных окон
 const closeModal = function(event) {
   const target = event.target;
+  // console.log('target: ', target);
   // console.log(this);
-  if (target.closest(`.modal__close`) || target === this) {
-    this.classList.add(`hide`);
-  }
-};
-
-// Закрытие по нажатию Esc
-const closeModalEsc = event => {
-  if (event.code === 'Escape') {
+  if (target.closest(`.modal__close`) || 
+    // target === this ||
+    target.type === 'submit' || // target.tagName === `FORM`
+    event.code === 'Escape'  // Закрытие по нажатию Esc
+  ) {
+    // this.classList.add(`hide`);
     modalItem.classList.add(`hide`);
     modalAdd.classList.add(`hide`);
-    document.removeEventListener('keydown', closeModalEsc); 
-  };
-};
+    document.removeEventListener('keydown', closeModal); 
 
+  }
+};
 
 
 // Открытие окна
@@ -51,7 +63,7 @@ addAd.addEventListener(`click`, () => {
   modalBtnSubmit.disabled = true;
   // Очищаем форму
   modalSubmit.reset();
-  document.addEventListener('keydown', closeModalEsc); 
+  document.addEventListener('keydown', closeModal); 
 });
 
 
@@ -59,6 +71,40 @@ addAd.addEventListener(`click`, () => {
 // Закрытие окон
 modalAdd.addEventListener(`click`, closeModal);
 modalItem.addEventListener(`click`, closeModal);
+
+
+
+// Контроль заполнения формы объявления
+const elementsModalSubmit = [...modalSubmit.elements]
+  .filter(it => it.tagName !== `BUTTON`);
+
+modalSubmit.addEventListener(`input`, () => {
+  // every проверяет все эл в массиве, если хотя бы 1 возвр false то вернёт false
+  const validForm = elementsModalSubmit.every( it => it.value);
+  console.log('validForm: ', validForm);
+
+  // если ВСЕ поля формы заполнены то разблокируем кнопку и убираем надпись
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? 'none' : ''; 
+});
+
+// НАЖАТИЕ НА ОТПРАВКУ ФОРМЫ
+modalSubmit.addEventListener(`submit`, event => {
+  event.preventDefault();
+  const itemObject = {};
+
+  for(const elem of elementsModalSubmit) {
+    itemObject[elem.name] = elem.value;
+
+  }
+
+  dataBase.push(itemObject);
+  modalSubmit.reset(); // Очищаем форму
+  // console.log('itemObject: ', itemObject);
+  closeModal(event);
+});
+
+
 
 
 
@@ -70,8 +116,10 @@ catalog.addEventListener(`click`, event => {
   // Проверяет есть ли данный класс и если нет поднимается выше
   if (target.closest(`.card`)) {
     modalItem.classList.remove(`hide`);
-    document.addEventListener('keydown', closeModalEsc); 
+    document.addEventListener('keydown', closeModal); 
   }
 });
+
+
 
 
